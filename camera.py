529d44e96f2d414cc99a7ac9da27cee622861f29
ZDavila3 <115113559+ZDavila3@users.noreply.github.com>
2024-02-23 22:52:44 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,10 +1,11 @@
 import cv2
-#from cvzone.HandTrackingModule import HandDetector
+from cvzone.HandTrackingModule import HandDetector
 
 
 cap = cv2.VideoCapture(0)  # Create a VideoCapture
-#detector = 
+detector = HandDetector(maxHands=1)
 while True:
     success, img = cap.read()
+    hands, img = detector.findHands(img)
     cv2.imshow("Imgae", img)
     cv2.waitKey(1)
